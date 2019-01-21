f94e3dd1242c4c3b2949f06805c90a89af0c7b77
Your Name <apmulet@example.com>
2019-01-21 09:26:31 +0100

    war mode changes

--- doorcheck/src/main/java/com/doorcheck/DoorcheckApplication.java
+++ doorcheck/src/main/java/com/doorcheck/DoorcheckApplication.java
@@ -2,12 +2,20 @@ package com.doorcheck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DoorcheckApplication {
+public class DoorcheckApplication /* extended for war */ extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DoorcheckApplication.class, args);
+	}
+	
+	/** added for war */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DoorcheckApplication.class);
 	}
 
 }
